59a751f7b2cb858d0578eba741c1fbe601b418f4
Marcus-Nastasi <vinnie.nsts@gmail.com>
2024-08-23 20:58:22 -0300

    merging

--- prisma/migrations/20240820143656_add_modal_color_enum/migration.sql
+++ prisma/migrations/20240820143656_add_modal_color_enum/migration.sql
@@ -8,4 +8,4 @@
 ALTER TABLE `aviso` ADD COLUMN `tipo_id` VARCHAR(191) NOT NULL;
 
 -- AddForeignKey
-ALTER TABLE `Aviso` ADD CONSTRAINT `Aviso_tipo_id_fkey` FOREIGN KEY (`tipo_id`) REFERENCES `tipos`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `aviso` ADD CONSTRAINT `Aviso_tipo_id_fkey` FOREIGN KEY (`tipo_id`) REFERENCES `tipos`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
