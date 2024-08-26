9af032d534085c71abcb671e100ca78698f6d551
gumends <gustavo08.mendes@gmail.com>
2024-08-26 11:05:51 -0300

    Web Socket Em busca de chamados

--- src/servicos/sockets/chamados.gateway.ts
+++ src/servicos/sockets/chamados.gateway.ts
@@ -1,7 +1,7 @@
 import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({cors: true})
 export class ChamadosGateway {
   @WebSocketServer() server: Server;
 
