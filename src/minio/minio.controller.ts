2b85a90f644e573743fce40e13fa0eb7039c365a
Marcus-Nastasi <vinnie.nsts@gmail.com>
2024-09-02 14:42:43 -0300

    refatorando funções do minio e tipos do ts para o minio

--- src/minio/minio.controller.ts
+++ src/minio/minio.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Controller, Get, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { MinioService } from './minio.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { IsPublic } from 'src/auth/decorators/is-public.decorator';
@@ -11,7 +11,7 @@ export class MinioController {
   @UseInterceptors(FileInterceptor('file'))
   @IsPublic()
   async create(@UploadedFile() file) {
-    const url = await this.minioService.uploadFile(process.env.MINIO_BUCKETNAME, file.originalname, file.buffer);
+    const url = await this.minioService.uploadFile(file.originalname, file.buffer, process.env.MINIO_BUCKETNAME);
     return { url };
   }
 
